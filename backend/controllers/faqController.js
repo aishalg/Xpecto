96ea28f85187cb88556ffa379610c4f47ccbe553
Aishal Gupta <aishalgupta3008@gmail.com>
2022-11-20 21:49:34 +0530

    Update faqController.js

--- backend/controllers/faqController.js
+++ backend/controllers/faqController.js
@@ -1,1 +1,96 @@
 const Faq = require("./../models/faqModel");
+
+
+exports.getAll= async(req, res, next) =>{
+    try{
+        const faqs= await Faq.find();
+
+        const context={
+            status:"success",
+            data: faqs,
+
+        };
+        res.status(200).json(context);
+
+    }
+   
+    catch(err){
+        res.status(400).json({Error: err});
+    }
+}
+
+// exports.updateFaqs= async(req, res, next) =>{
+//     try{
+//         const Faqs= await Faqs.findOne(
+//             {_id:req.params.id}
+//         );
+//         const context={
+//             status:"success",
+//             data: Faqs,
+//         };
+//         res.status(200).json(context);
+//     }
+//     catch(err){
+//         res.status(400).json({Error: err});
+//     }
+// }
+exports.addFaqs= async(req, res, next)=> {
+    try{
+        const newData={
+            
+                question: 
+                   req.body.question,
+                    
+                
+                answer: req.body.answer,
+            
+        }
+        const upd= await Faqs.insertOne( newData)
+    }
+    catch(err){
+        res.status(400).json({Error: err});
+    }
+};
+
+
+
+
+exports.newUpdate= async(req, res, next)=> {
+    try{
+        const newData={
+            $set:{
+                question: 
+                   req.body.question,
+                    
+                
+                answer: req.body.answer,
+            }
+        }
+        const upd= await Faqs.updateOne({_id:req.params.id}, newData)
+    }
+    catch(err){
+        res.status(400).json({Error: err});
+    }
+};
+
+exports.deleteFaqs= async(req, res, next)=> {
+    try{
+       
+        const upd= await Faqs.deleteOne({_id:req.params.id})
+    }
+    catch(err){
+        res.status(400).json({Error: err});
+    }
+};
+
+exports.deleteAllFaqs= async(req, res, next)=> {
+    try{
+      
+        const upd= await Faqs.deleteMany({})
+    }
+    catch(err){
+        res.status(400).json({Error: err});
+    }
+};
+
+
