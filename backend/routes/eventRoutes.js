a7756cfeb0952b28e330ec6f082a47782cb90335
KaushikV <kaushik.vishwakarma2003@gmail.com>
2022-11-21 02:11:24 +0530

    Events Api

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {} = require("./../controllers/eventController");
+const eventController = require("./../controllers/eventController");
 
+router.route("/").get(eventController.getAll);
+router.route("/add").post(eventController.addevent)
+router.route("/update/:id").post(eventController.updateEvent)
+router.route("/delete/:id").post(eventController.deleteOneEvent)
+router.route("/deleteall").post(eventController.deleteAllEvent)
 module.exports = router;
