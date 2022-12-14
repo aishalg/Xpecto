8bf6c877ee88bd4f9625b21cbd76f46a423c0ce2
Aishal Gupta <aishalgupta3008@gmail.com>
2022-12-14 15:05:41 +0530

    Update AddFaq.jsx

--- client/src/pages/admin/Faq/AddFaq.jsx
+++ client/src/pages/admin/Faq/AddFaq.jsx
@@ -12,19 +12,64 @@ const AddFaq = () => {
       answer:""
     } 
     
-    return <>
-   
-    <div className={style["faqbutton"]}>
+     const [newdata,setnewdata]=useState(data)
+     
+      const handleSubmit=async(e)=>{
+        e.preventDefault();
+        const data=newdata;
+
+        try {
+          const url=`${process.env.REACT_APP_BACKENDURL}/faq/events/`;
+          const res=await axios.post(url,data);
+        } catch (error) {
+          console.log("ref eroor")
+        }
+      }
+      
+         const handlechange=({currentTarget:input})=>{
+      if(input.name ==="question" ){
+        const ques=newdata.question;
+     
+        setnewdata({...newdata,ques:question})
+
+      }else{
+        setnewdata({...newdata,[input.name]:input.value});
+
+      }
+    }
+      
+      
     
-  
-    <Popup trigger={<Button variant="outlined" style={{marginLeft:"20px"}} >Add Faq</Button>
+   return <>
+    <div className={style.header} style={{marginTop:"20px"}}>
+    
+    <Popup trigger={<Button variant="outlined" style={{marginLeft:"20px"}} modal nested >Add Events</Button>
 } position="bottom center">
-    
+  {close=> (
+    <Stack component="form"  style={{marginLeft:"15px"}} onSubmit={handleSubmit} className={style.form} marginTop={5}>     
+        <div >
+  <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} rowSpacing={2}  padding={5}justifyContent="center" display="flex">
+    <Grid  item xs={4}sm={4} md={4}display="flex" justifyContent="center" alignItems="center">  <TextField id="outlined-basic" size="small" label="Question" value={newdata.question} onChange={handlechange}name="question"variant="outlined" className={style.textfield}/>
+</Grid>
+    <Grid  item xs={4}sm={4} md={4} display="flex" justifyContent="center" alignItems="center">  <TextField id="outlined-basic" size="small" label="Answer" value={newdata.answer} name="answer"onChange={handlechange}variant="outlined" className={style.textfield}/>
+</Grid>
    
+<Grid item xs={8}sm={12} md={12}display="flex" justifyContent="center" alignItems="center"></Grid>
+
+
+<Grid  item display="flex" justifyContent="flex-end" alignItems="center" xs={12} sm={12} md={12}>  <Button variant="outlined" 
+onClick={handleSubmit}style={{marginTop:"20px",marginBottom:"30px",width:"300px",display:"flex",flexDirection:"column",justifyContent:"flex-end"}} >Submit</Button>
+<Grid  item display="flex" justifyContent="center" alignItems="center" xs={12} sm={12} md={12}>  <Button variant="outlined" 
+onClick={()=>{close()}}style={{marginTop:"20px",marginBottom:"30px",width:"300px",display:"flex",flexDirection:"column",justifyContent:"center"}} >Close</Button></Grid>
+</Grid>
+  </Grid>
+  </div>
+
+</Stack>
+  )}
   </Popup>
   </div> 
         </>
 };
-
 export default AddFaq;
 
