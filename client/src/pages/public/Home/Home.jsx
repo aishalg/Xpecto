e2603266309bf45d7ede6bbd69aa75102d340d94
Sarvesh Jadhav <sarveshjadhav543@gmail.com>
2023-01-17 20:30:13 +0530

    add sidebar open and close

--- client/src/pages/public/Home/Home.jsx
+++ client/src/pages/public/Home/Home.jsx
@@ -6,52 +6,64 @@ import { useState,useEffect } from "react";
 import axios from "axios";
 import Razorpay from "../component/payment/Razorpay";
 import { useNavigate } from "react-router-dom";
+import Sidebar from "../component/Sidebar/Sidebar";
 const Home = () => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
     const navigate=useNavigate();
-	const getUser = async () => {
-		try {
-			const url = `${process.env.REACT_APP_BACKENDURL}/auth/login`;
-			const { data } = await axios.get(url, { withCredentials: true });
-			// console.log("data",data)
+  const getUser = async () => {
+    try {
+      const url = `${process.env.REACT_APP_BACKENDURL}/auth/login`;
+      const { data } = await axios.get(url, { withCredentials: true });
+      // console.log("data",data)
             if(data.isnewuser){
-                navigate("/signup");
-			}
+        navigate("/signup");
+      }
 
 			setUser(data.message,data.error);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	useEffect(() => {
-		getUser();
-	}, []);
+  useEffect(() => {
+    getUser();
+  }, []);
 
-    // const usera = userDetails.user;
-	const logout = () => {
-		window.open(`${process.env.REACT_APP_BACKENDURL}/auth/logout`, "_self");
-	};
-    const googleAuth = () => {
-		window.open(
-			`${process.env.REACT_APP_BACKENDURL}/auth/google/callback`,
-			"_self"
-		);
-	};
+  // const usera = userDetails.user;
+  const logout = () => {
+    window.open(`${process.env.REACT_APP_BACKENDURL}/auth/logout`, "_self");
+  };
+  const googleAuth = () => {
+    window.open(
+      `${process.env.REACT_APP_BACKENDURL}/auth/google/callback`,
+      "_self"
+    );
+  };
 	console.log("usedetail " ,user)
-    return (
-        <>
-        <div className={styles["page"]}>
-            <Link to="/admin/dashboard">Go to Admin Dashboard</Link>
-        </div>
+  return (
+    <>
+      <div
+        style={{
+          position: "fixed",
+          height: "70px",
+          width: "70px",
+          display: "flex",
+		  zIndex: "2",
+        }}
+      >
+        <Sidebar />
+      </div>
+      <div className={styles["page"]}>
+        <Link to="/admin/dashboard">Go to Admin Dashboard</Link>
+      </div>
         <Button variant="outlined" onClick={googleAuth} sx={{ m: 5 }}>Sign in google</Button>
         <Button variant="outlined" onClick={googleAuth} sx={{ m: 5 }}>Login with google</Button>
         <Button variant="outlined" onClick={logout} sx={{ m: 5 }}>Logout</Button>
-		<div>
+      <div>
 			<Razorpay/>
-		</div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Home;
---
+++ client/src/pages/public/component/Sidebar/Sidebar.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        className={styles.sidebarToggle}
+        onClick={() => setSidebarOpen((prev) => !prev)}
+      >
+        <svg
+          className={`${styles.sidebarIcon} ${
+            sidebarOpen ? styles.sidebarIconChange : ""
+          }`}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+        >
+          <line
+            x1="10"
+            y1="24"
+            x2="90"
+            y2="24"
+            stroke="black"
+            strokeWidth={10}
+          />
+          <line
+            x1="10"
+            y1="50"
+            x2="90"
+            y2="50"
+            stroke="black"
+            strokeWidth={10}
+          />
+          <line
+            x1="10"
+            y1="76"
+            x2="90"
+            y2="76"
+            stroke="black"
+            strokeWidth={10}
+          />
+        </svg>
+      </button>
+      <div
+        className={`${styles.sidebarBackdrop} ${
+          sidebarOpen ? styles.sidebarBackdropOpen : ""
+        }`}
+      >
+        <div
+          className={`${styles.sidebarContainer} ${
+            sidebarOpen ? styles.sidebarOpen : ""
+          }`}
+        >
+          {/* --animation-order species order of animations */}
+          <button
+            style={{ "--animation-order": 1 }}
+            className={styles.sidebarBtn}
+          >
+            HOME
+          </button>
+          <button
+            style={{ "--animation-order": 2 }}
+            className={styles.sidebarBtn}
+          >
+            ABOUT US
+          </button>
+          <button
+            style={{ "--animation-order": 3 }}
+            className={styles.sidebarBtn}
+          >
+            TIMELINE
+          </button>
+          <button
+            style={{ "--animation-order": 4 }}
+            className={styles.sidebarBtn}
+          >
+            SPONSORS
+          </button>
+          <button
+            style={{ "--animation-order": 5 }}
+            className={styles.sidebarBtn}
+          >
+            EVENTS
+          </button>
+          <button
+            style={{ "--animation-order": 6 }}
+            className={styles.sidebarBtn}
+          >
+            WORKSHOPS
+          </button>
+          <button
+            style={{ "--animation-order": 7 }}
+            className={styles.sidebarBtn}
+          >
+            FAQ
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
