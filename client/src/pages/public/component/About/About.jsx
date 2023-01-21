07b5e851309a4962f273212441e193e52bdfa3cf
Sarvesh Jadhav <sarveshjadhav543@gmail.com>
2023-01-21 22:30:40 +0530

    about us content and css fix

--- client/src/pages/public/component/About/About.jsx
+++ client/src/pages/public/component/About/About.jsx
@@ -35,22 +35,33 @@ export default function About() {
         ref={aboutRef}
         data-color="#1AF527"
         className={style["aboutcontainer"]}
-      > 
+      >
         <p className={style["heading1"]}>ABOUT US</p>
         <p className={style["aboutcontent"]}>
           XPECTO’23 is the second edition of IIT Mandi’s afresh Inter College
           Technical Fest organized by the Science and Technical Council (SNTC),
-          IIT Mandi. XPECTO’22 envisioned a platform where our nation’s astute
+          IIT Mandi. 
+          <br />
+          <br />
+          XPECTO’22 envisioned a platform where our nation’s astute
           and beaming minds could unite and dazzle in discussions and
           competitions relating to diverse science, technology, and management
-          domains. After the astounding participation of 7200+ contestants from
+          domains.
+          <br /> 
+          <br /> 
+          After the astounding participation of 7200+ contestants from
           650+ colleges across India, it is back with another thunderous
-          edition. XPECTO’23 now flaunts a plethora of offline events, where the
-          contestants will be accommodated in IIT Mandi. XPECTO’23 aspires to be
-          the greatest offline convention of the brightest contestants in the
+          edition. 
+          <br />
+          <br />
+          XPECTO’23 now flaunts a plethora of offline events, where the
+          contestants will be accommodated in IIT Mandi. 
+          <br />
+          <br />
+          XPECTO’23 aspires to be the greatest offline convention of the brightest contestants in the
           country, working their way through intense events ranging from
           hackathons, contests, and conferences, all topped with an icing of
-          entertaining shows and events
+          entertaining shows and events.
         </p>
       </div>
     </>
