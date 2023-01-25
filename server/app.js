7e6ecebab85a79e7f3427c86ef22c95c6be72ee9
Rajat Verma <rajat311201@gmail.com>
2023-01-25 23:49:35 +0530

    cors patch

--- server/app.js
+++ server/app.js
@@ -8,64 +8,38 @@ const sponsorRouter = require("./routes/sponsorRoutes");
 const teamMemberRouter = require("./routes/teamMemberRoutes");
 const webinarRouter = require("./routes/webinarRoutes");
 const workshopRouter = require("./routes/workshopRoutes");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const passportSetup=require("./config/passport");
-const authRoute=require("./routes/authRouters");
-const paymentRouter=require("./routes/paymentRouters")
-const userRouter =require("./routes/userRoute")
+
+const paymentRouter = require("./routes/paymentRouters");
+const userRouter = require("./routes/userRoute");
 // INITIALIZING EXPRESS APP
 const app = express();
 
 
-
-// use it before all route definitions
-// app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(cors());
-app.use(cors({origin:'*'}))
+app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://xpecto-rajat-client.vercel.app");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  })
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_LINK);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 // Development logging
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
-
-
-
-
-//Cookies session
-// app.use(cookieSession({
-//     name:"session",
-//     keys:["xpectoiitmandi"],
-//     maxAge:24*60*60*100,
-// }))
-
-// app.use(passport.initialize());
-// app.use(passport.session()); 
-
-// app.use(
-//     cors({
-//         // origin:"http://localhost:3000",
-//         methods:"GET,POST,PUT,DELETE,PATCH",
-//         credentials:true,
-//     })
-// )
-
-
 
 // DEFINING ALL ROUTES
 
-
-app.use("/auth",authRoute);
-app.use("/api/payment/",paymentRouter);
+app.use("/api/payment/", paymentRouter);
 app.use("/api/events", eventRouter);
 app.use("/api/faqs", faqRouter);
 app.use("/api/keytalks", keytalkRouter);
@@ -73,13 +47,15 @@ app.use("/api/sponsors", sponsorRouter);
 app.use("/api/teamMembers", teamMemberRouter);
 app.use("/api/webinars", webinarRouter);
 app.use("/api/workshops", workshopRouter);
-app.use("/api/user",userRouter);
+app.use("/api/user", userRouter);
+
+
 // IF A ROUTE NOT AVAILABLE
 app.all("*", async (req, res) => {
-    res.status(404).json({
-        status: "fail",
-        message: "Route doesn't exist",
-    });
+  res.status(404).json({
+    status: "fail",
+    message: "Route doesn't exist",
+  });
 });
 
 module.exports = app;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,74 +1,7 @@
 const { OAuth2Client } = require("google-auth-library");
 const jwt = require("jsonwebtoken");
-const passport=require("passport");
-const User = require("../models/userModel")
+const User = require("../models/userModel");
 const { promisify } = require("util");
-exports.loginsuccess = async (req, res) => {
-  if (req.user) {
-    // console.log("user id:               ",req.user.emails[0].value)
-    const userinfo = await User.findOne({ email: req.user.emails[0].value });
-    // console.log("userinfo", userinfo);
-    var newuser = false;
-    const userdetail = req.user;
-    if (userinfo == null) {
-      // console.log(userdetail,"user")
-      User.create({
-        googleId: userdetail.id,
-        email: userdetail.emails[0].value,
-        displayName: userdetail.displayName,
-        firstName: userdetail.name.givenName,
-        image: userdetail.photos[0].value,
-      });
-      newuser = true;
-      // console.log("User data save succesfully")
-    }
-    const context = {
-      email: userdetail.emails[0].value,
-      displayName: userdetail.displayName,
-      firstName: userdetail.name.givenName,
-      image: userdetail.photos[0].value,
-    };
-    res.status(200).json({
-      error: false,
-      isnewuser: newuser,
-      message: "Successfully Loged In",
-      data: context,
-    });
-    // console.log(req.user.id,"user")
-  } else {
-    res.status(403).json({
-      error: true,
-      message: "Login Unsuccessfully Please retry after some time",
-    });
-  }
-};
-
-exports.loginfail = (req, res) => {
-  res.status(401).json({
-    error: true,
-    message: "log in failure",
-  });
-};
-
-exports.google = () => {
-  passport.authenticate("google", { scope: ["profile", "email"] });
-};
-
-exports.logout = (req, res) => {
-  req.logOut();
-  res.send(200).json({
-    status: "success",
-    message: "successfully logged out",
-  });
-};
-
-exports.googlecallback = () => {
-  passport.authenticate("google", {
-    successRedirect: process.env.CLIENT_LINK,
-    failureRedirect: "/login/failed",
-  });
-};
-
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -115,7 +48,7 @@ exports.login = async (req, res, next) => {
     createSendToken(userFind, 200, res, isNewUser);
   } catch (err) {
     console.log(err);
-    res.status(404).json({ status: "error occured" });
+    res.status(404).json({ status: "error occured", message: err });
   }
 };
 
