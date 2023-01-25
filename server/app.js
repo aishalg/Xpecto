90f3cec958a16ff6ccb2080baafa8f1f3c48c03a
Rajat Verma <rajat311201@gmail.com>
2023-01-25 20:27:11 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cors({origin:'*'}))
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://xpecto-rajat-client.vercel.app");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
